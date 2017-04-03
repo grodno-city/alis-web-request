9b580bba68cc9f09591af7e6ddbdd8bda981ba5a
Demyanchik Vadim <vadim.demyanchik@gmail.com>
2017-04-03 16:18:36 +0300

    Delete static require config

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 const request = require('request');
-const SESSION_ID = require('./config.json').SESSION_ID;
-
-const j = request.jar();
-
-j.setCookie(
-  request.cookie(`sessionalis=${SESSION_ID}`),
-  'http://86.57.174.45/pls/alis/StartEK/index.php'
-);
 
 function sendInitialQuery(query, callback) {
-  const START_URL = `http://86.57.174.45/alis/EK/do_searh.php?radiodate=simple&valueINP=${query.year}&tema=ALL&tag=ALL`;
+  const START_URL = `http://${query.ip}/alis/EK/do_searh.php?radiodate=simple&valueINP=${query.year}&tema=1&tag=6`;
+  const j = request.jar();
+  j.setCookie(
+    request.cookie(`sessionalis=${query.cookieId}`),
+    'http://86.57.174.45/pls/alis/StartEK/index.php'
+  );
   request({ url: START_URL, jar: j }, (err, response, body) => {
     if (err) {
       callback(err);
       return;
     }
-    callback(null, body);
+    callback(null, { page: body, jar: j });
   });
 }
 
-function getPage(url, callback) {
-  request({ url, jar: j }, (err, response, body) => {
+function getPage(query, callback) {
+  request({ url: query.url, jar: query.jar }, (err, response, body) => {
     if (err) {
       callback(err);
       return;
@@ -29,6 +26,4 @@ function getPage(url, callback) {
   });
 }
 
-sendInitialQuery({ year: 2016 }, (err, body) => {
-  console.log(body);
-});
+module.exports = sendInitialQuery;
