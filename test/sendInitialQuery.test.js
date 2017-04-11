32292d93e46a838a4e28fe34e52a5aca7863fcc9
Demyanchik Vadim <vadim.demyanchik@gmail.com>
2017-04-11 16:46:49 +0300

    Add reply set-cookie

--- test/sendInitialQuery.test.js
+++ test/sendInitialQuery.test.js
@@ -3,21 +3,25 @@ import { expect } from 'chai';
 import { sendInitialQuery } from '../index';
 
 describe('method sendInitialQuery', () => {
-  it('should exists as a public method', (done) => {
+  it('should exists as a public method', () => {
     expect(typeof sendInitialQuery).to.equal('function');
-    done();
   });
 
-  it('should makes http call to alis library and return html string with jar object', (done) => {
+  it('should makes http call to alis library and return html string and set cookie in jar', (done) => {
     const origin = 'http://86.57.174.45';
     const resource = '/alis/EK/do_searh.php?radiodate=simple&valueINP=2016&tema=1&tag=6';
+    const cookiesString = 'sessionid=38afes7a7';
+
     nock(origin)
       .get(resource)
-      .reply(200, '<html></html>');
+      .reply(200, '<html></html>', {
+        'Set-Cookie': 'sessionid=38afes7a7',
+      });
 
     sendInitialQuery({ year: 2016, ip: '86.57.174.45' }, (err, data) => {
+      const cookies = data.jar.getCookieString(`${origin}${resource}`);
       expect(data.page).to.equal('<html></html>');
-      expect(typeof data.jar).to.equal('object');
+      expect(cookies).to.equal(cookiesString);
       done();
     });
   });
