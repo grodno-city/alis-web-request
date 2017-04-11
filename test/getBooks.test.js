be3bd26b18b5851e40ebc21bb363ff995c9a3ce1
Demyanchik Vadim <vadim.demyanchik@gmail.com>
2017-04-11 16:43:51 +0300

    Update test getBooks

--- test/getBooks.test.js
+++ test/getBooks.test.js
@@ -4,16 +4,16 @@ import { expect } from 'chai';
 import { getBooks } from '../index';
 
 describe('method getBooks', () => {
-  it('should exists as a public method', (done) => {
+  it('should exists as a public method', () => {
     expect(typeof getBooks).to.equal('function');
-    done();
   });
 
-  it('should return books', (done) => {
+  it('should return title of book', (done) => {
     const page = fs.readFileSync('./test/test.html');
     const $ = cheerio.load(page);
-    const books = $('.article');
-    expect(getBooks($)).to.eql(books);
+    const book = getBooks($);
+    const title = 'Black, K. Игрушки-мультяшки от Katrin Black : мастер-классы и выкройки / [Katrin Black]. - Санкт-Петербург ';
+    expect(book[0]).to.eql(title);
     done();
   });
 });
