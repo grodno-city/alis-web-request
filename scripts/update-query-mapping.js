d6a982d6b5ebeca025deaaf167075b9bf5f670ca
HannaKazello <hanna.kazello@gmail.com>
2017-06-13 17:39:39 +0300

    now does not refer to index.js #52

--- scripts/update-query-mapping.js
+++ scripts/update-query-mapping.js
@@ -1,22 +1,20 @@
 import request from 'request';
-import fs from 'fs';
-import { getPage, parsePage } from '../index.js';
+import cheerio from 'cheerio';
 
 function getTypes($, type) {
   const obj = {};
-  const types = $('[name=' + type + ']').children().map((i, el) => {
+  $(`[name=${type}]`).children().map((i, el) => {
     const str = $(el).text();
     obj[str] = $(el).attr('value');
   });
   return obj;
 }
 // http://86.57.174.45/pls/alis/StartEK/index.php
-const j = request.jar();
-getPage({ url: 'http://86.57.174.45/pls/alis/EK/simple.php', jar: j }, (err, body) => {
-  const $ = parsePage(body);
+request({ url: 'http://86.57.174.45/pls/alis/EK/simple.php' }, (err, res, body) => {
+  const $ = cheerio.load(body);
   const recordTypes = getTypes($, 'tema');
   const queryTypes = getTypes($, 'tag');
   const qM = { recordType: recordTypes, queryType: queryTypes };
   const str = JSON.stringify(qM);
-  fs.writeFileSync('./queryMap.js', str);
-})
+  process.stdout.write(str);
+});
