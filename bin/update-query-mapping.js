74805e6ec15af454a6af4a97cb441c740969ce7d
HannaKazello <hanna.kazello@gmail.com>
2017-06-14 14:10:57 +0300

    Add update-query-mapping script (#54)
    * add update-query-mapping script
    
    * now does not refer to index.js #52
    
    * rename scripts folder to bin #52
    
    * add grep command to update-query-mapping field
    
    * remove grep command from update-query-mapping field
    
    * pass in indentation level as 2

---
+++ bin/update-query-mapping.js
@@ -0,0 +1,20 @@
+import request from 'request';
+import cheerio from 'cheerio';
+
+function getTypes($, type) {
+  const obj = {};
+  $(`[name=${type}]`).children().map((i, el) => {
+    const str = $(el).text();
+    obj[str] = $(el).attr('value');
+  });
+  return obj;
+}
+// http://86.57.174.45/pls/alis/StartEK/index.php
+request({ url: 'http://86.57.174.45/pls/alis/EK/simple.php' }, (err, res, body) => {
+  const $ = cheerio.load(body);
+  const recordTypes = getTypes($, 'tema');
+  const queryTypes = getTypes($, 'tag');
+  const qM = { recordType: recordTypes, queryType: queryTypes };
+  const str = JSON.stringify(qM, null, 2);
+  process.stdout.write(str);
+});
