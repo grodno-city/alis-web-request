cf4e537813f4ab17cd7f6aa1ac72b4347820f9f8
HannaKazello <hanna.kazello@gmail.com>
2017-06-21 17:15:36 +0300

    add getRecordByID and getRecordInfo tests

---
+++ src/test/getRecordByID.test.js
@@ -0,0 +1,23 @@
+import nock from 'nock';
+import { expect } from 'chai';
+import { getRecordByID } from '../index';
+
+describe('getRecordByID', () => {
+  it('should be a function', () => {
+    expect(typeof getRecordByID).to.equal('function');
+  });
+  it('return error if record not found', (done) => {
+    const alisEndpoint = 'http://86.57.174.45';
+    const id = '0';
+    const firstPageUrl = `/alis/EK/do_view.php?id=${id}`;
+
+    nock(alisEndpoint)
+      .get(firstPageUrl)
+      .reply(200, '<html> Undefined variable </html>');
+
+    getRecordByID(alisEndpoint, id, (err, info) => {
+      expect(err.message).to.equal('Record not found');
+      done();
+    });
+  });
+});
