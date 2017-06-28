ac26056b167037e16766c7d4110feb7cb432aa53
HannaKazello <hanna.kazello@gmail.com>
2017-06-28 15:59:13 +0300

    add new functions for getRecordInfo,

--- src/index.js
+++ src/index.js
@@ -146,7 +146,7 @@ export function collectReferences(table) {
   if (title !== 'Ссылки на др. биб.записи') {
     return [];
   }
-  let references = [];
+  const references = [];
   const data = table.children().toArray();
   data.shift();
   data.forEach((el) => {
@@ -159,41 +159,70 @@ export function collectReferences(table) {
   return references;
 }
 
-export function collectFund(data) {
-  const fund = {};
-  for (let i = 0; i < data[0].length; i += 1) {
-    if (data[0][i].match('фондир')) {
-      fund[data[0][i]] = data[1][i];
-    }
+export function collectFunds(table) {
+  const title = table.children().toArray()[0].children[1].children[0].data;
+  if (title !== 'Фонд') {
+    return [];
   }
-  return fund;
+  const funds = [];
+  const data = table.children().toArray();
+  data.shift();
+  data.forEach((el) => {
+    funds.push({
+      name: el.children[0].children[0].children[0].data,
+      count: el.children[1].children[0].data,
+    });
+  });
+  return funds;
+}
+
+export function collectInfo(table) {
+  const title = table.children().toArray()[0].children[1].children[0].data;
+  if (title !== 'Название') {
+    return [];
+  }
+  const tags = [];
+  const data = table.children().toArray();
+  data.shift();
+  data.forEach((el) => {
+    tags.push({
+      tag: el.children[0].children[0].children[0].data,
+      field: el.children[1].children[0].data,
+    });
+  });
+  return tags;
+}
+
+export function collectYears(table){
+  const title = table.children().toArray()[0].children[1].children[0].data;
+  if (title !== 'Год(комплект)') {
+    return [];
+  }
+  const references = [];
+  const data = table.children().toArray();
+  data.shift();
+  data.forEach((el) => {
+    const a = el.children[0].children[0];
+    references.push(a.children[0].data.substr(0, 4));
+  });
+  return references;
 }
 
 export function getRecordInfo($) {
   const info = {};
-  const firstTable = $('table').first().parsetable(false, false, true);
-  for (let i = 0; i < firstTable[0].length; i++) {
-    if (firstTable[0][i] !== '') {
-      if (firstTable[0][i] === 'ISBN') {
-        info[firstTable[0][i]] = firstTable[1][i].split(', ');
-      } else info[firstTable[0][i]] = firstTable[1][i];
-    }
-  }
+  const firstTable = $('table').first();
+  info.tags = collectInfo(firstTable);
+  info.belMarkId = $('span')[0].children[0].data;
   const secondTable = $('table').first().next('table');
   if (secondTable) {
-    const second = secondTable.parsetable(false, false, true);
-    if (second[0][0] === 'Фонд') {
-      info['Фонд'] = collectFund(second);
-    } else {
-      info.tags = collectReferences(second);
-    }
+    // info.funds = collectFunds(secondTable);
+    info.years = collectYears(secondTable);
   }
 
   const thirdTable = $('table').first().next('table').next('table');
   if (thirdTable) {
-    info.tags = collectReferences(thirdTable);
+    info.references = collectReferences(thirdTable);
   }
-  delete info['Название'];
   return info;
 }
 
@@ -202,7 +231,7 @@ export function getRecordByID(alisEndpoint, id, callback) {
   const INITIAL_URL = `${alisEndpoint}${firstPageUrl}`;
   getPage({ url: INITIAL_URL }, (err, body) => {
     if (err) return callback(err);
-    if (body.match('Undefined variable')) return callback(new Error('Record not found'))
+    if (body.match('Undefined variable')) return callback(new Error('Record not found'));
     const $ = parsePage(body);
     const info = getRecordInfo($);
     info.id = id;
