1dc4f23471546b373cf03f07d8e1c4f7e8c96f27
HannaKazello <hanna.kazello@gmail.com>
2017-06-30 12:18:53 +0300

    add new page, fix repetitive parts

--- src/test/scripts/update-record-fixtures.js
+++ src/test/scripts/update-record-fixtures.js
@@ -1,16 +1,18 @@
 import request from 'request';
 import fs from 'fs';
 
-const options = {
-  pageWithYears: 28392,
-  pageWithFunds: 21989,
-  alisEndpoint: 'http://86.57.174.45',
-};
-
-request({ url: `${options.alisEndpoint}/alis/EK/do_view.php?id=${options.pageWithYears}` }, (err, res, body) => {
-  if (!err) fs.writeFileSync('./src/test/fixtures/recordWithYears.html', body);
-});
-
-request({ url: `${options.alisEndpoint}/alis/EK/do_view.php?id=${options.pageWithFunds}` }, (err, res, body) => {
-  if (!err) fs.writeFileSync('./src/test/fixtures/recordWithFunds.html', body);
+const alisEndpoint = 'http://86.57.174.45';
+const pages = [
+  { tag: 'WithYears', id: 28392 },
+  { tag: 'WithFunds', id: 21989 },
+  { tag: 'WithEmptyFieldsValue', id: 46273 },
+];
+pages.forEach((el) => {
+  request({ url: `${alisEndpoint}/alis/EK/do_view.php?id=${el.id}` }, (err, res, body) => {
+    if (!err) {
+      fs.writeFileSync(`./src/test/fixtures/record${el.tag}.html`, body);
+    } else {
+      console.log(err);
+    }
+  });
 });
