dd77628b79c0e644d9e89a9213e640149a5a972e
HannaKazello <hanna.kazello@gmail.com>
2017-06-30 13:48:10 +0300

    fix collectFields call

--- src/test/collectFields.test.js
+++ src/test/collectFields.test.js
@@ -15,13 +15,13 @@ describe('collectFields', () => {
   it('shoud return an array', () => {
     const $ = cheerio.load(record);
     const fieldsTable = getTable($, 'fields');
-    const fields = collectFields(fieldsTable);
+    const fields = collectFields($, fieldsTable);
     expect(fields).to.be.an('array');
   });
   it('each arrays element shoud be { tag: String, value: String }', () => {
     const $ = cheerio.load(record);
     const fieldsTable = getTable($, 'fields');
-    const fields = collectFields(fieldsTable);
+    const fields = collectFields($, fieldsTable);
     fields.forEach((el) => {
       expect(el).to.include.all.keys('tag', 'value');
     });
@@ -29,7 +29,7 @@ describe('collectFields', () => {
   it('shoud procces table even value in fields table is empty', () => {
     const $ = cheerio.load(recordWithEmptyFieldsValue);
     const fieldsTable = getTable($, 'fields');
-    const fields = collectFields(fieldsTable);
+    const fields = collectFields($, fieldsTable);
     expect(fields.some(el => el.value === '')).to.be.true;
   });
 });
