60d7c68ff8779191fef7a6dd86c7a862bcdbf67a
Pavel Zubkou <pavel.zubkou@gmail.com>
2021-10-14 00:00:07 +0300

    refactor: move example to bin

--- bin/get-record-by-id.js
+++ bin/get-record-by-id.js
@@ -1,9 +1,13 @@
-import { getRecordByID } from './src/index';
+// DEBUG=alis-web-request yarn babel-node bin/get-record-by-id.js [ID]
+
+import { getRecordByID } from '../src/index';
 
 const options = {
-  id: 28392,
+  id: process.argv[2],
+  // Электронны каталог https://grodnolib.by/
   alisEndpoint: 'http://86.57.174.45',
 };
+
 getRecordByID(options.alisEndpoint, options.id, (err, record) => {
   if (err) {
     console.log(err.message);
